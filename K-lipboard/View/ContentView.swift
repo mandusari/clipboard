3878c1ed1c34cbe8794541a92c1d4f260c4ba2ca
만두_김용태 <giyom83@naver.com>
2023-03-27 22:14:45 +0900

    숏컷 만드는중.

--- K-lipboard/View/ContentView.swift
+++ K-lipboard/View/ContentView.swift
@@ -19,60 +19,67 @@ struct ContentView: View {
 
     @State var cancellable: Set<AnyCancellable> = Set<AnyCancellable>()
     @State private var showingAlert = false
+
+    @State private var selectionItem: Item? = nil
     
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
+            List(items, id:\.self, selection: $selectionItem) { item in
+//                ForEach(items) { item in
                     NavigationLink {
                         VStack {
                             Text("\(item.stringData ?? "")")
                         }
                     } label: {
-                        SideCell(pinAction: {
-                            dataController.dataUpdate()
-                        }, item: item)
-                        .contextMenu {
-                            VStack {
-                                Text(item.stringData ?? "")
-                                Divider()
-                                Button(action: {
-                                    dataController.delete(item: item)
-                                }) {
-                                    Text("삭제")
-                                        .foregroundColor(.red)
-                                        .fontWeight(.bold)
-                                }
-                            }
+                        VStack {
+                            SideCell(pinAction: {
+                                dataController.dataUpdate()
+                            }, item: item)
                         }
+//                        .contextMenu {
+//                            VStack {
+//                                Text(item.stringData ?? "")
+//                                Divider()
+//                                Button(action: {
+//                                    dataController.delete(item: item)
+//                                }) {
+//                                    Text("삭제")
+//                                        .foregroundColor(.red)
+//                                        .fontWeight(.bold)
+//                                }
+//                            }
+//                        }
+                        
                     }
-                }
-                .onDelete(perform: delete)
+//                }
             }
             .toolbar {
-                ToolbarItemGroup(placement: .navigation) {
-                    Button {
-                        showingAlert = true
-                    } label: {
-                        Label("Delete all clips", systemImage: "trash")
-                    }
-                    .alert("경고", isPresented: $showingAlert) {
-                        Button("삭제", role: .destructive) {
-                            deleteAll()
+                ToolbarItem {
+                    HStack {
+                        Button {
+                            showingAlert = true
+                        } label: {
+                            Label("Delete all clips", systemImage: "trash")
                         }
-                    } message: {
-                        Text("모두 삭제하시겠습니까?")
+                        .alert("경고", isPresented: $showingAlert) {
+                            Button("삭제", role: .destructive) {
+                                deleteAll()
+                            }
+                        } message: {
+                            Text("모두 삭제하시겠습니까?")
+                        }
+                        
+                        Button {
+                            dataController.delete(item: selectionItem!)
+                        } label: {
+                            Label("Delete clip", systemImage: "delete.left")
+                        }
                     }
                 }
             }
         }
     }
-    
-    private func delete(offsets: IndexSet) {
-        let item = offsets.map{ items[$0]}
-        dataController.delete(item: item.first!)
-    }
-    
+        
     private func deleteAll() {
         items.forEach { item in
             if item.isPin == false {
--- K-lipboard/View/StatusBarList.swift
+++ K-lipboard/View/StatusBarList.swift
@@ -17,19 +17,34 @@ struct StatusBarList: View {
     private var pasteboard = NSPasteboard.general
 
     var body: some View {
-        ForEach(items) { item in
+        ForEach(items.indices, id: \.self) { index in
             Button {
                 // 선택한 텍스트를 보드에 다시 집어넣음.
                 pasteboard.clearContents()
-                pasteboard.setString(item.stringData ?? "", forType: .string)
+                pasteboard.setString(items[index].stringData ?? "", forType: .string)
                 
                 // command+v 단축키로 텍스트를 현재 화면에 붙여넣음.
                 let event = CGEvent(keyboardEventSource: nil, virtualKey: CGKeyCode(0x09), keyDown: true)
                 event?.flags = CGEventFlags.maskCommand
                 event?.post(tap: .cghidEventTap)
             } label: {
-                Text(item.stringData ?? "")
+                let stringData = (items[index].stringData ?? "")
+                Text(stringData.trimmingCharacters(in: .whitespaces).prefix(15))
             }
+            .customShortcut(index)
+//            .keyboardShortcut(KeyEquivalent(Character("\(index)")), modifiers: [.command, .shift])
         }
+    }
+    
+}
+
+extension View {
+    
+    public func customShortcut(_ index: Int) -> some View {
+        if index < 10 {
+            return self.keyboardShortcut(KeyEquivalent(Character("\(index)")), modifiers: [.command, .shift])
+        }
+        
+        return AnyView(self)
     }
 }
