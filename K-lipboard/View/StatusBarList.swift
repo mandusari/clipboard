cb9cee20b61f140e9f9418dfc252401a1ed14e73
만두_김용태 <giyom83@naver.com>
2023-04-16 12:22:37 +0900

    Hotkey로 라이브러리 변경.. 여전히 동작은 안됨.

--- K-lipboard/View/StatusBarList.swift
+++ K-lipboard/View/StatusBarList.swift
@@ -6,24 +6,10 @@
 //
 
 import SwiftUI
-import CoreData
-import Combine
-import KeyboardShortcuts
-
-extension KeyboardShortcuts.Name {
-    static let one = Self("one", default: .init(.one, modifiers: [.command, .option]))
-    static let two = Self("two", default: .init(.two, modifiers: [.command, .option]))
-    static let three = Self("three", default: .init(.three, modifiers: [.command, .option]))
-    static let four = Self("four", default: .init(.four, modifiers: [.command, .option]))
-    static let five = Self("five", default: .init(.five, modifiers: [.command, .option]))
-    static let six = Self("six", default: .init(.six, modifiers: [.command, .option]))
-    static let seven = Self("seven", default: .init(.seven, modifiers: [.command, .option]))
-    static let eight = Self("eight", default: .init(.eight, modifiers: [.command, .option]))
-    static let nine = Self("nine", default: .init(.nine, modifiers: [.command, .option]))
-    static let zero = Self("zero", default: .init(.zero, modifiers: [.command, .option]))
-}
+import HotKey
 
 struct StatusBarList: View {
+    @Environment(\.managedObjectContext) var dbContext
     @FetchRequest(
         sortDescriptors: [
             NSSortDescriptor(keyPath: \Item.isPin, ascending: false),
@@ -32,17 +18,25 @@ struct StatusBarList: View {
         ],
         animation: .default)
     var items: FetchedResults<Item>
-    private var pasteboard = NSPasteboard.general
+    let hotkeys: [HotKey] = [HotKey(key: .zero, modifiers: [.control, .command]),
+                            HotKey(key: .one, modifiers: [.control, .command]),
+                             HotKey(key: .two, modifiers: [.control, .command]),
+                             HotKey(key: .three, modifiers: [.control, .command]),
+                             HotKey(key: .four, modifiers: [.control, .command]),
+                             HotKey(key: .five, modifiers: [.control, .command]),
+                             HotKey(key: .six, modifiers: [.control, .command]),
+                             HotKey(key: .seven, modifiers: [.control, .command]),
+                             HotKey(key: .eight, modifiers: [.control, .command]),
+                             HotKey(key: .nine, modifiers: [.control, .command])]
 
     var body: some View {
         ForEach(items.indices, id: \.self) { index in
             
             let item = items[index]
-
             Button {
                 // 선택한 텍스트를 보드에 다시 집어넣음.
-                pasteboard.clearContents()
-                pasteboard.setString(item.stringData ?? "", forType: .string)
+                NSPasteboard.general.clearContents()
+                NSPasteboard.general.setString(item.stringData ?? "", forType: .string)
             } label: {
                 let stringData = (item.stringData ?? "")
                 HStack {
@@ -57,45 +51,31 @@ struct StatusBarList: View {
                 }
             }
             .customShortcut(index, isPin: item.isPin) { key in
-                debugPrint("KEYVALUE = \(key)")
-                let name = convertTo(key: key)
-                KeyboardShortcuts.onKeyDown(for: name) {
-                    pasteboard.clearContents()
-                    debugPrint(item.stringData ?? "")
-                    pasteboard.setString(item.stringData ?? "", forType: .string)
-                    let event = CGEvent(keyboardEventSource: nil, virtualKey: CGKeyCode(0x09), keyDown: true)
-                    event?.flags = CGEventFlags.maskCommand
-                    event?.post(tap: .cghidEventTap)
-                }
+                debugPrint("key = \(key)")
+                self.hotkeys[key].keyDownHandler = keyDownAction(clipText: item.stringData ?? "")
             }
         }
     }
     
-    private func convertTo(key: Int) -> KeyboardShortcuts.Name {
-        switch key {
-        case 0: return .zero
-        case 1: return .one
-        case 2: return .two
-        case 3: return .three
-        case 4: return .four
-        case 5: return .five
-        case 6: return .six
-        case 7: return .seven
-        case 8: return .eight
-        case 9: return .nine
-        default:
-            return .one
+    func keyDownAction(clipText: String) -> () -> () {
+        return {
+            NSPasteboard.general.clearContents()
+            debugPrint(clipText)
+            NSPasteboard.general.setString(clipText, forType: .string)
+            let event = CGEvent(keyboardEventSource: nil, virtualKey: CGKeyCode(0x09), keyDown: true)
+            event?.flags = CGEventFlags.maskCommand
+            event?.post(tap: .cghidEventTap)
         }
     }
 }
 
 extension View {
     
-    func customShortcut(_ index: Int, isPin: Bool, keyRegisterHandler: (Int) -> Void) -> some View {
+    func customShortcut(_ index: Int, isPin: Bool, keyRegisterHandler: @escaping (Int) -> ()) -> some View {
         if index < 10 && isPin == true {
-            let keyValue = (index+1) % 10
+            let keyValue = (index) % 10
             keyRegisterHandler(keyValue)
-            return AnyView(self.keyboardShortcut(KeyEquivalent(Character("\(keyValue)")), modifiers: [.option, .command]))
+            return AnyView(self.keyboardShortcut(KeyEquivalent(Character("\(keyValue)"))/*, modifiers: [.control, .command]*/))
         }
         
         return AnyView(self)
